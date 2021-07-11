c35f6e72e219758158bc3057e3a43ea242cd8644
kiwibrie <brianne0128@gmail.com>
2021-07-11 16:56:03 -0500

    top menu bar (file, edit, help) finished

--- src/main/java/ucf/assignments/ListManagerApplicationController2.java
+++ src/main/java/ucf/assignments/ListManagerApplicationController2.java
@@ -17,19 +17,17 @@ import java.util.Objects;
 public class ListManagerApplicationController2 {
     ToDoList masterlist = new ToDoList("My List");
 
-    @FXML public MenuItem About;
-    @FXML public TextField DirectoryTextBox;
-    @FXML public Button SaveOK;
-    @FXML public Button CancelButton;
-
     //---------------------------------------------------------- TOP TOOLBAR
     //FILE
+    @FXML public MenuItem CloseApp;
     @FXML
     public void CloseAppClicked(ActionEvent actionEvent) {
         Platform.exit();
     }
 
     //HELP
+    @FXML public MenuItem About;
+
     @FXML
     public void AboutClicked(ActionEvent actionEvent) {
         try {
@@ -47,6 +45,10 @@ public class ListManagerApplicationController2 {
     }
 
     //EDIT
+    @FXML public MenuItem SaveList;
+    @FXML public MenuItem LoadList;
+    @FXML public MenuItem DeleteList;
+
     @FXML
     public void SaveListClicked(ActionEvent actionEvent) {
         try {
@@ -63,9 +65,10 @@ public class ListManagerApplicationController2 {
         }
     }
 
+    @FXML
     public void LoadListClicked(ActionEvent actionEvent) {
         try {
-            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("SaveScene.fxml")));
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("LoadScene.fxml")));
             Scene scene = new Scene(root);
             Stage stage = new Stage();
 
@@ -79,22 +82,38 @@ public class ListManagerApplicationController2 {
     }
 
     public void DeleteListClicked(ActionEvent actionEvent) {
+        masterlist.ClearList();
+        //todo update listviewer
     }
 
     //---------------------------------------------------------- SAVE SCENE
+    @FXML public Button SaveOK;
+    @FXML public Button CancelButton;
+    @FXML public TextField DirectoryTextBox;
+
+    @FXML
     public void SaveOKClicked(ActionEvent actionEvent) {
         String path = DirectoryTextBox.getText();
         masterlist.SaveList(path);
+        //todo update listviewer
+        Stage toClose = (Stage) SaveOK.getScene().getWindow();
+        toClose.close();
     }
 
+    @FXML
     public void CancelClicked(ActionEvent actionEvent) {
         Stage toClose = (Stage) CancelButton.getScene().getWindow();
         toClose.close();
     }
 
     //---------------------------------------------------------- LOAD SCENE
-    public void LoadOKClicked(ActionEvent actionEvent) {
-        String path = DirectoryTextBox.getText();
+    @FXML public Button LoadOK;
 
+    @FXML public void LoadOKClicked(ActionEvent actionEvent) {
+        String path = DirectoryTextBox.getText();
+        masterlist.LoadList(path);
+        //todo update listviewrr
+        Stage toClose = (Stage) LoadOK.getScene().getWindow();
+        toClose.close();
     }
 }
