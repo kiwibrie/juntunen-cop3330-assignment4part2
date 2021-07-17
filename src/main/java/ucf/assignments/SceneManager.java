8e9125c73db337a8ba6d23b0dd41e88575f40c17
Brianne Juntunen <84480365+kiwibrie@users.noreply.github.com>
2021-07-16 20:45:58 -0500

    added copyright comments and killed venti. rest in peace barbatos.

--- src/main/java/ucf/assignments/SceneManager.java
+++ src/main/java/ucf/assignments/SceneManager.java
@@ -1,5 +1,10 @@
 package ucf.assignments;
 
+/*
+ *  UCF COP3330 Summer 2021 Assignment 4 Solution
+ *  Copyright 2021 Brianne Juntunen
+ */
+
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
--- src/main/java/ucf/assignments/ToDoListController.java
+++ src/main/java/ucf/assignments/ToDoListController.java
@@ -1,5 +1,10 @@
 package ucf.assignments;
 
+/*
+ *  UCF COP3330 Summer 2021 Assignment 4 Solution
+ *  Copyright 2021 Brianne Juntunen
+ */
+
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
